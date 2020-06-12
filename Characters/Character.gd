bf4a42660b5e4c166191a1447f63fdc776e3d639
Jack Teo <jinkiateo91@github.com>
2020-06-13 11:40:00 +1200

    update

--- Characters/Character.gd
+++ Characters/Character.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 signal shoot
-signal health_changed
+#signal health_changed
 signal dead
 
 export (PackedScene) var Bullet
@@ -9,17 +9,19 @@ export (int) var speed
 export (int) var health
 export (float) var gun_cooldown
 
+var initial_health = null
 var velocity = Vector2()
 var can_shoot = true
-var alive = true
+var isHurt = false
 
 func _ready():
 	$GunTimer.wait_time = gun_cooldown
-	$Body.play("default")
+	initial_health = health
 
 func _physics_process(delta):
 	var motion = velocity.normalized() * speed
 	move_and_slide(motion, velocity)
+	
 
 func shoot():
 	if can_shoot:
@@ -29,13 +31,31 @@ func shoot():
 		emit_signal('shoot', Bullet, $Body/Muzzle.global_position, dir)
 
 func take_damage(amount):
+	isHurt = true
+	$Body/isHurt_Timer.stop()
+	$Body/isHurt_Timer.start()
 	health -= amount
-	$Body.play("isHurt")
+	$Body/isHurt_anime.play("isHurt")
 	if health <= 0:
 		queue_free()
+	elif health <= 40:
+		$Body/isHurt_anime.get_animation("isHurt").length = 0.5
+		$Body/isHurt_anime.get_animation("isHurt").step = 0.25
+	print(health)
 
 func _on_GunTimer_timeout():
 	can_shoot = true
 
-func _on_Body_animation_finished():
-	$Body.play("default")
+func _on_isHurt_Timer_timeout():
+	isHurt = false
+	$Body/notHurt_Timer.stop()
+	$Body/notHurt_Timer.start()
+
+func _on_notHurt_Timer_timeout():
+	if isHurt == false:
+		health = initial_health
+		$Body/isHurt_anime.stop()
+		$Body/isHurt_anime.seek(0, true)
+		$Body/isHurt_anime.get_animation("isHurt").length = 1
+		$Body/isHurt_anime.get_animation("isHurt").step = 0.5
+		print("full")
